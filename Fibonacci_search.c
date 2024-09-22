04e867267e1d8b410d1bb27edc8a247c9d4149ea
SAMALA VISHNU TEJA <018Vishnuteja@gmail.com>
2024-09-22 18:24:00 +0530

    Update Fibonacci_search.c

--- Fibonacci_search.c
+++ Fibonacci_search.c
@@ -6,29 +6,21 @@ int min(int a,int b)
 }
 int fibonacciSearch(int a[], int n, int key)
 {
-    // Initialize Fibonacci numbers
-    int fib2 = 0;  // (m-2)'th Fibonacci number
-    int fib1 = 1;  // (m-1)'th Fibonacci number
-    int fibM = fib1 + fib2;  // m'th Fibonacci number
 
-    // Find the smallest Fibonacci number greater than or equal to n
+    int fib2 = 0;  
+    int fib1 = 1; 
+    int fibM = fib1 + fib2;  
+    
     while (fibM < n) 
     {
         fib2 = fib1;
         fib1 = fibM;
         fibM = fib1 + fib2;
     }
-
-    // Marks the eliminated range from front
     int offset = -1;
-
-    // While there are elements to be inspected, compare with the Fibonacci numbers
     while (fibM > 1)
     {
-        // Check if fib2 is a valid location
         int i = min(offset + fib2, n - 1);
-
-        // If key is greater than the value at index fib2, cut the subarray from offset to i
         if (a[i] < key)
         {
             fibM = fib1;
@@ -36,27 +28,21 @@ int fibonacciSearch(int a[], int n, int key)
             fib2 = fibM - fib1;
             offset = i;
         }
-        // If key is smaller, cut the subarray after i+1
         else if (a[i] > key)
         {
             fibM = fib2;
             fib1 = fib1 - fib2;
             fib2 = fibM - fib1;
         }
-        // Key found
         else
         {
             return i;
         }
     }
-
-    // Comparing the last element with the key
     if (fib1 && a[offset + 1] == key)
     {
         return offset + 1;
     }
-
-    // If the element is not found, return -1
     return -1;
 }
 int main()
@@ -72,9 +58,8 @@ int main()
     {
         scanf("%d",&a[i]);    
     }
-
     printf("\n");
-
+    
     printf("Enter the key value: ");
     scanf("%d",&key);
 
@@ -88,6 +73,5 @@ int main()
     {
         printf("...........Element Not found...........\n");
     }
-
     return 0;
 }
